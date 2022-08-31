9a16ee45c4895de26df784bb7998d1f81251f1e5
Nina Guo <ninaguo1305@gmail.com>
2022-08-31 21:00:23 +0800

    Add Multiplier initislly

---
+++ src/main/java/Multiplier.java
@@ -0,0 +1,16 @@
+public class Multiplier {
+
+
+
+    public static void main(String[] args){
+        int firstNumber = 5;
+        int secondNumber = 44;
+
+        int result = Multiplier(firstNumber, secondNumber);
+       }
+
+    public static int multiply(int value1,int value2) {
+         return value1*value2;
+    }
+
+}
